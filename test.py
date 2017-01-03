022b58f0f4bb0b23ad9692f28c0fd01912c80eeb
Andrea Simes <avs2151@columbia.edu>
2017-01-02 16:06:16 -0800

    making template more pretty

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 # need to import flask 
 # run pip install Flask
 
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, request, render_template, g, redirect, Response
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run()
