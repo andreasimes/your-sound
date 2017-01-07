0a699975c6bbdc4593acf8e7d8d9c841cd7ee795
Andrea Simes <avs2151@columbia.edu>
2017-01-06 20:28:33 -0800

    cleaning up and adding comments

--- test.py
+++ test.py
@@ -17,6 +17,10 @@ from unicodedata import normalize
 with open('data.json') as json_data_file:
 	data = json.load(json_data_file)
 
+# links to config files
+CLIENT_ID = data['spotify']['id'] 
+CLIENT_SECRET = data['spotify']['secret']
+
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates') # creates paths for Jinja templates in a templates folder
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -26,65 +30,50 @@ app = Flask(__name__, template_folder=tmpl_dir)
 @app.route("/", methods=['GET','POST']) # initial landing page
 def index():
 	if request.method == 'POST':
-		print('hi')
 		query = request.form['text']
-		print(query)
 		
 		return redirect(url_for('.results', query=query))
-		#return redirect(url_for('.moby', query=query))
-		#return redirect(url_for('.results'))
 	
 	elif request.method == 'GET':
 		return render_template('index.html');
 
 
-
-CLIENT_ID = data['spotify']['id']
-CLIENT_SECRET = data['spotify']['secret']
-
 @app.route("/results/<query>") # results handling
 #@app.route("/moby/<query>")
 def results(query):
+
+	# request to spotify API
+
+	# generate token via client credentials auth flow
 	var = {'grant_type':'client_credentials'}
-	#req = make_response(render_template('https://accounts.spotify.com/api/token', params=var))
 	encoded = base64.b64encode(CLIENT_ID+":"+CLIENT_SECRET)
-	#resp = flask.Response("https://accounts.spotify.com/api/token")
-	#req.headers['Authorization']=["Basic " + encoded]
 	headers = {'Authorization':'Basic '+encoded}
-	
-	#print("78f7c13de8b44da2b92b70e2235cb316:4e64b9b4da8d4276ac86e1de7c994f3a".encode("utf-8"))
-	#print(headers)
-	#print(json.dumps(var))
 	r = requests.post('https://accounts.spotify.com/api/token', data=var, headers=headers)
-	##print(r.headers)
-	#return r.text, r.raise_for_status()
 	result = r.text
-
 	token = ast.literal_eval(result)['access_token']
-
 	input = {'q':query,'type':'track', 'limit': '10'}
-	r = requests.get('https://api.spotify.com/v1/search', params=input)
+	r = requests.get('https://api.spotify.com/v1/search', params=input) # request
 
-	#url = 'https://api.spotify.com/v1/audio-features/'
 	
-
-	#print(r.encode('ascii','ignore'))
 	parsed_json = json.loads(r.text)
-	#song = parsed_json['tracks']['items'][0]['name']
+	#song = parsed_json['tracks']['items'][0]['name'] # parsing for song
+	
+	# initialize dictionary to store tracks + info
 	songdict = {}
-	#print(type(len(parsed_json['tracks']['items'])))
 	name = ""
 	uri = ""
 	trackid = ""
+
+	# iterate over all tracks
+	# and access spotify audio features endpoint to return danceability info for each track
 	for i in range(0,len(parsed_json['tracks']['items'])):
+
+		# isolate song info
 		name = parsed_json['tracks']['items'][i]['name']
 		uri = parsed_json['tracks']['items'][i]['uri']
 		trackid = parsed_json['tracks']['items'][i]['id']
 		
-		#first_artist_genres = parsed_json['tracks']['items'][0]['artists'][0]['genres']
-		#print(name)
-		#print(uri)
-		#print(trackid)
+		
 		imp={'id':trackid}
 		dat = {}
 		headers = {'Authorization': 'Bearer ' + token }
@@ -92,70 +81,60 @@ def results(query):
 		re = requests.get(address, data=dat, headers=headers)
 		new_json = json.loads(re.text)
 		danceability = new_json['danceability']
+		songdict[name]=[uri, danceability] # update dict track's info
 
-		songdict[name]=[uri, danceability] #update dict track's info
-
+	# separate dictionary for songs with danceability > 0.5 --> more danceable
 	display_dict = {}
 	for key, value in songdict.iteritems():
 		if value[1] > 0.5:
 			display_dict[key]=[value[0],value[1]]
-	#print(display_dict)
 
-	#artist = parsed_json['tracks']['items'][0]['artists'][0]['name']
-	#link = parsed_json['tracks']['items'][0]['uri']
-	#return render_template('index.html',name=name)
-	#return render_template('results.html',song=song, artist=artist, link=link, songdict=songdict)
+	#artist = parsed_json['tracks']['items'][0]['artists'][0]['name'] # parsing for artist 
+	#link = parsed_json['tracks']['items'][0]['uri'] # parsing for link
+
 	return render_template('results.html',songdict=display_dict)
-#def moby(query):
+
 
 @app.route("/moby", methods=['GET']) 
 def moby():
 	return render_template('moby_dance_party.html') # handling for moby's dance party tab -- initial display
 
+
 @app.route("/mobyparty/<query>", methods=['POST']) # return results for moby's dance party after voice input
 def mobyparty(query):
-	var = {'grant_type':'client_credentials'}
-	#req = make_response(render_template('https://accounts.spotify.com/api/token', params=var))
-	encoded = base64.b64encode(CLIENT_ID+":"+CLIENT_SECRET)
-	#resp = flask.Response("https://accounts.spotify.com/api/token")
-	#req.headers['Authorization']=["Basic " + encoded]
-	headers = {'Authorization':'Basic '+encoded}
-	
-	#print("78f7c13de8b44da2b92b70e2235cb316:4e64b9b4da8d4276ac86e1de7c994f3a".encode("utf-8"))
-	#print(headers)
-	#print(json.dumps(var))
-	r = requests.post('https://accounts.spotify.com/api/token', data=var, headers=headers)
-	##print(r.headers)
-	#return r.text, r.raise_for_status()
-	result = r.text
 
+	#API request for credentials
+	var = {'grant_type':'client_credentials'}
+	encoded = base64.b64encode(CLIENT_ID+":"+CLIENT_SECRET) # encoding specified by spotify
+	headers = {'Authorization':'Basic '+encoded}
+	r = requests.post('https://accounts.spotify.com/api/token', data=var, headers=headers)
+	result = r.text
 	token = ast.literal_eval(result)['access_token']
 
+	#API GET query to Spotify's search endpoint
 	query = query + ' year:1970-1972'+' genre:soul'
-	#query = 'love' + ' year:1970-1972'+' genre:soul'
+
 	input = {'q':query,'type':'track', 'limit': '10'}
 	r = requests.get('https://api.spotify.com/v1/search', params=input)
-
-	#url = 'https://api.spotify.com/v1/audio-features/'
+	parsed_json = json.loads(r.text)
+	#song = parsed_json['tracks']['items'][0]['name'] # parsing for song
 	
 
-	#print(r.encode('ascii','ignore'))
-	parsed_json = json.loads(r.text)
-	#song = parsed_json['tracks']['items'][0]['name']
+	# initialize dictionary to hold track info
 	songdict = {}
-	#print(type(len(parsed_json['tracks']['items'])))
 	name = ""
 	uri = ""
 	trackid = ""
+
+
+	# iterate over all tracks returned
+	# and access audio features API to get info for danceability
+	# and store their info in dictionary
 	for i in range(0,len(parsed_json['tracks']['items'])):
+
 		name = parsed_json['tracks']['items'][i]['name']
 		uri = parsed_json['tracks']['items'][i]['uri']
 		trackid = parsed_json['tracks']['items'][i]['id']
-		
-		#first_artist_genres = parsed_json['tracks']['items'][0]['artists'][0]['genres']
-		#print(name)
-		#print(uri)
-		#print(trackid)
 		imp={'id':trackid}
 		dat = {}
 		headers = {'Authorization': 'Bearer ' + token }
@@ -163,19 +142,17 @@ def mobyparty(query):
 		re = requests.get(address, data=dat, headers=headers)
 		new_json = json.loads(re.text)
 		danceability = new_json['danceability']
-
 		songdict[name]=[uri, danceability] #update dict track's info
 
+
+	# handling to separate tracks with danceability score > 0.5 --> more danceable
 	display_dict = {}
 	for key, value in songdict.iteritems():
 		if value[1] > 0.5:
 			display_dict[key]=[value[0],value[1]]
-	#print(display_dict)
+	
 
-	#artist = parsed_json['tracks']['items'][0]['artists'][0]['name']
-	#link = parsed_json['tracks']['items'][0]['uri']
-	#return render_template('index.html',name=name)
-	#return render_template('results.html',song=song, artist=artist, link=link, songdict=songdict)
+	
 	return render_template('moby.html',songdict=display_dict)
 
 if __name__ == "__main__":
